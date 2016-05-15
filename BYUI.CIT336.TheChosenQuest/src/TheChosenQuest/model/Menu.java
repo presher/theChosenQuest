a2152173f5d0623b1df87a9b7277ffa17e79d0b6
Jason <Jason@Lenovo-PC.attlocal.net>
2016-05-14 22:29:18 -0500

    Modified By Jason

--- BYUI.CIT336.TheChosenQuest/src/TheChosenQuest/model/Menu.java
+++ BYUI.CIT336.TheChosenQuest/src/TheChosenQuest/model/Menu.java
@@ -5,57 +5,12 @@
  */
 package TheChosenQuest.model;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+
 
 /**
  *
  * @author Jason
  */
-public class Menu implements Serializable{
-    List<Menu> Menu;
+public class Menu {
     
-    public Menu(){
-        Menu = new ArrayList<>();
-    }
-
-    public List<Menu> getMenu() {
-        return Menu;
-    }
-
-    public void setMenu(List<Menu> Menu) {
-        this.Menu = Menu;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + Objects.hashCode(this.Menu);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Menu other = (Menu) obj;
-        if (!Objects.equals(this.Menu, other.Menu)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Menu{" + "Menu=" + Menu + '}';
-    }
 }
