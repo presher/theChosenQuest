60103256591bd1fde3ebee7bd764c573edfcd33a
Jason <Jason@Lenovo-PC.attlocal.net>
2016-05-28 18:36:44 -0500

    Modified By Jason and Jeremy

--- BYUI.CIT336.TheChosenQuest/src/TheChosenQuest/control/inventoryControl.java
+++ BYUI.CIT336.TheChosenQuest/src/TheChosenQuest/control/inventoryControl.java
@@ -5,10 +5,42 @@
  */
 package TheChosenQuest.control;
 
+import TheChosenQuest.model.Inventory;
+
 /**
  *
  * @author Jason
  */
-public class inventoryControl {
-    
+
+
+
+public class inventoryControl extends Inventory {
+   public void amountOfInventory(int Boots, int Armor, int gold, int Weapons){
+        if(gold < 0 && Boots < 0 && Armor < 0 && Weapons < 0){ //Empty Inventory
+            System.out.println("You have no inventory");
+        }
+        if(gold > 0 && Boots < 0 && Armor < 0 && Weapons < 0){
+            System.out.println("\t\nYou have Gold " + gold);
+        }
+        if(gold > 0 && Boots > 0 && Armor < 0 && Weapons < 0){
+            System.out.println("\t\nYou have gold, " + gold +  " and Boots " + Boots);
+        }
+        if(gold > 0 && Boots > 0 && Armor > 0 && Weapons < 0){
+            System.out.println("\t\nYou have Gold, " + gold + " and Boots, " + Boots + " and Armor " + Armor);
+        }
+        if(gold > 0 && Boots > 0 && Armor > 0 && Weapons > 0){
+            System.out.println("\t\nYou have Gold " + gold + " and Boots " + Boots + " , and Armor " + Armor + " , and Weapons " + Weapons);
+        }
+        double inventoryTotal = Boots + Armor + gold + Weapons;
+         System.out.println(inventoryTotal);
+   }
+public void addToInventory(int Boots, int Armor, int Weapons, int gold){
+
+
+}
+
+public void removeFromInventory(double Boots, double Armor, double Weapons, double gold){
+
+}
+
 }
